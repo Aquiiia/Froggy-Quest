15195301ff3ef429e7144c07b21169d06a40000f
kepc2 <mureliuserik@gmail.com>
2024-09-04 13:18:49 +0200

    minor fixes

--- froggy/froggy.gd
+++ froggy/froggy.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-signal hit
-
 @export var speed = 400
 
 func get_input():
@@ -12,16 +10,6 @@ func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 
-
-
 func start(pos):
 	position = pos
 	show()
-	$CollisionShape2D.disabled = false
-
-
-func _body_hit(_body: Node2D) -> void:
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
