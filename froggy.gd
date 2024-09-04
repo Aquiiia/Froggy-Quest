7effaf5723ab002ccffe548a3dbae2d401bafa1a
kepc2 <mureliuserik@gmail.com>
2024-09-04 12:40:57 +0200

    Froggy and enemy

---
+++ froggy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+signal hit
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
+
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
+func _body_hit(_body: Node2D) -> void:
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
