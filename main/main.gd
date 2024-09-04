2d368190dafb5f800bc1859d01bf6810a6c63975
kepc2 <mureliuserik@gmail.com>
2024-09-04 15:40:17 +0200

    HUD_test

--- main/main.gd
+++ main/main.gd
@@ -8,14 +8,18 @@ var score
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Froggy.start($StartPos.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
@@ -47,4 +51,4 @@ func _on_mob_timer_timeout():
 	add_child(mob)
 
 func _ready():
-	new_game()
+	pass
