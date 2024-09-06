6caf7616738490ca6b971fff22a65f8023ab3d62
Aquiiia <beatrice@familjenosterberg.se>
2024-09-06 15:35:16 +0200

    knappt något

--- TopFrog/top_frog.gd
+++ TopFrog/top_frog.gd
@@ -1,10 +1,22 @@
 extends CharacterBody2D
 @export var speed = 150
 
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			get_input()
+		elif event.is_action_released():
+			velocity = null
+		
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-
+	$PointLight2D/Pic.set_rotation_degrees(input_direction.angle()*180/PI)
+	
+	print(input_direction.angle())
+	
 func _physics_process(_delta):
-	get_input()
+	
 	move_and_slide()
