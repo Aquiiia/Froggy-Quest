06876b04b34ad2c31e4d6cc0a498106d83f8f383
kepc2 <mureliuserik@gmail.com>
2024-09-09 15:05:48 +0200

    fixed gameover func

---
+++ goon/mob_spawner.gd
@@ -0,0 +1,28 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+func _on_mob_timer_timeout():
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	# Set the mob's position to a random location.
+	mob.position = mob_spawn_location.position
+
+	# Add some randomness to the direction.
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
+	mob.add_to_group("enemies")
+
+func _remove_mobs():
+	for mob in get_tree().get_nodes_in_group("enemies"):
+		mob.queue_free()
