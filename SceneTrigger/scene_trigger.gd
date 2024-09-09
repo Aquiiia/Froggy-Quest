920c442ee2a94cb04acfabf134ac44624fb879f7
Aquiiia <beatrice@familjenosterberg.se>
2024-09-09 15:25:46 +0200

    eh=?

--- SceneTrigger/scene_trigger.gd
+++ SceneTrigger/scene_trigger.gd
@@ -3,7 +3,7 @@ class_name SceneTrigger extends Area2D
 @export var connected_scene: String
 var scene_folder = "res://scenes/"
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body):
 	var full_path = scene_folder + connected_scene + ".tscn"
 	var scene_tree = get_tree()
-	scene_tree.change_scene_to_file(full_path)
+	scene_tree.call_deferred("change_scene_to_file", full_path)
--- TopFrog/top_frog.gd
+++ TopFrog/top_frog.gd
@@ -5,24 +5,28 @@ extends CharacterBody2D
 @onready var sprite = $PointLight2D/Pic
 
 func _unhandled_input(event):
+	#print(event)
+	#print(velocity)
 	if event is InputEventKey:
-		if event.pressed:
+		if event.pressed: #Fortsätt att byta bild osv när du har speed, men speeden byts efter event
 			get_input(true)
 		elif event.is_action_released:
 			get_input(false)
 			
 	
 func get_input(pressed):
+	
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	
-	if pressed:
+	if  velocity.x != 0 or velocity.y != 0 or pressed:
 		$PointLight2D/Pic.set_rotation_degrees(input_direction.angle()*180/PI)
 		ap.play("Jump")
 	
+	
 	#print(input_direction.angle())
 	#print($PointLight2D/Pic.get_rotation_degrees())
-	#print()
+	#print(velocity, velocity.x, velocity.y)
 	
 func _physics_process(_delta):
 	move_and_slide()
