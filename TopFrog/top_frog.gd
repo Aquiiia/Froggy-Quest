0db85d06e44b0e6c7673c6905f5031baf881412c
Aquiiia <beatrice@familjenosterberg.se>
2024-09-09 10:14:55 +0200

    TopFrog DOne

--- TopFrog/top_frog.gd
+++ TopFrog/top_frog.gd
@@ -1,22 +1,25 @@
 extends CharacterBody2D
 @export var speed = 150
 
-
-
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed:
-			get_input()
-		elif event.is_action_released():
-			velocity = null
+			get_input(true)
+		elif event.is_action_released:
+			get_input(false)
+			
 		
-func get_input():
+func get_input(pressed):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-	$PointLight2D/Pic.set_rotation_degrees(input_direction.angle()*180/PI)
 	
-	print(input_direction.angle())
+	if pressed:
+		$PointLight2D/Pic.set_rotation_degrees(input_direction.angle()*180/PI)
+	
+	#print(input_direction.angle())
+	#print($PointLight2D/Pic.get_rotation_degrees())
+	#print()
 	
 func _physics_process(_delta):
-	
 	move_and_slide()
+	
