4d2a7dd72710053ca94cc7cc6ed752830763e57d
kepc2 <mureliuserik@gmail.com>
2024-09-11 16:49:20 +0200

    queen fly shell added

--- goon/enemy_ai/state_machine.gd
+++ goon/enemy_ai/state_machine.gd
@@ -1,42 +1,18 @@
-extends Node
+extends Node2D
 
-class_name StateMachine
+var current_state: State
+var previous_state: State
 
-var current_state: Object
 
-var history = []
-var states = {}
-
-# fsm short for finite state machine
-# The Initial State is set to the first child Node
 func _ready():
-	for state in get_children():
-		state.fsm = self
-		states[state.name] = state
-		if current_state:
-			remove_child(state)
-		else:
-			current_state = state
+	current_state = get_child(0) as State
+	previous_state = current_state
 	current_state.enter()
 
-# Adds the previous state into the history list
-func change_state(state_name):
-	history.append(current_state.name)
-	set_state(state_name)
 
-# Checks if there are any elements in history
-func back():
-	if history.size() > 0:
-		# Removes and returns the last element of the array
-		set_state(history.pop_back())
-
-func set_state(state_name):
-	# Removes the current state node from the scene tree
-	remove_child(current_state)
-	
-	# Adds the new state to the scene tree
-	current_state = states[state_name]
-	add_child(current_state)
-	
-	# Calls the states enter function
+func change_state(state):
+	current_state = find_child(state) as State
 	current_state.enter()
+	
+	previous_state.exit()
+	previous_state = current_state
