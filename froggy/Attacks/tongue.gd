71fc8cb7a747a4d013e2179403bc9fe972b314d2
Eli4z05 <Eliaz.andersson@gmail.com>
2024-09-13 11:34:08 +0200

    DAMAGE

--- froggy/Attacks/tongue.gd
+++ froggy/Attacks/tongue.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 	position += velocity * delta
 
 func _ready():
-	connect("area_entered", Callable(self, "_on_tongue_body_entered"))
+	connect("body_entered", Callable(self, "_on_tongue_body_entered"))
 
 func _on_tongue_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
