901792d4a001fe461e389065b5ff3fac2c433b91
Erik Hägg Murelius <mureliuserik@gmail.com>
2025-01-23 15:55:16 +0100

    boss fight less broken

--- goon/queen_fly.gd
+++ goon/queen_fly.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal boss_dmged
+
 @onready var player = get_parent().find_child("Froggy")
 @onready var sprite = $Sprite2D
 @onready var progress_bar = $UI/ProgressBar
@@ -10,15 +12,15 @@ var health = 100:
 	set(value):
 		health = value
 		progress_bar.value = value
+		
 		if value <= 0:
 			progress_bar.visible = false
 			find_child("StateMachine").change_state("Death")
 
 func _ready():
-	set_physics_process(false) 
+	set_physics_process(false)
 
 func _process(_delta):
-	
 	direction = player.position - position
 	
 	if direction.x < 0:
@@ -26,11 +28,10 @@ func _process(_delta):
 	else:
 		sprite.flip_h = true
 
-
 func _physics_process(delta):
-	if player:
-		velocity = direction.normalized() * 40
-		move_and_collide(velocity * delta)
+	velocity = direction.normalized() * 40
+	move_and_collide(velocity * delta)
 
 func take_damage(amount: int):
 	health -= amount
+	boss_dmged.emit()
---
+++ scenes/final_scene.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+func game_over() -> void:
+	$HUD.show_game_over()
+	Global.player_data["health"] = 10
+	Global.player_data["xp"] = 0
+	Global.upgrades_bars["ssb"] = 424
+	Global.upgrades_bars["sdb"] = 424
+	Global.upgrades_bars["tsb"] = 424
+	Global.upgrades_bars["tdb"] = 424
+	Global.upgrade_btns["ss"] = 0
+	Global.upgrade_btns["sd"] = 0
+	Global.upgrade_btns["ts"] = 0
+	Global.upgrade_btns["td"] = 0
+	Global.player_abilities["spit_cooldown"] = 0.8
+	Global.player_abilities["spit_damage"] = 5
+	Global.player_abilities["tongue_cooldown"] = 3
+	Global.player_abilities["tongue_damage"] = 8
+
+
+
+func _ready() -> void:
+	var player = get_parent().find_child("Froggy")
+	player.connect("froggy_killed", Callable(self, "game_over"))
+	player.xp = Global.player_data["xp"]
+	player.hp = Global.player_data["health"] #hämtar datan för alla frogs
+	player.xp = Global.player_data["xp"] #hämtar datan för alla frogs
+	player.position = $StartPos.position
+	
+	var healthbar = get_node("health_bar")
+	var health_progress = healthbar.get_child(0)
+	health_progress.value = Global.player_data["health"]
